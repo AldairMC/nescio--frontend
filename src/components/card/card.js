ed68d286b1b0342c5e55979e74dfd270043cba21
Nicolas Camacho <ncamacho1@cuc.edu.co>
2020-03-29 19:55:13 -0500

    :lipstick: Style struture and general folders defined

---
+++ src/components/card/card.js
@@ -0,0 +1,13 @@
+import React from 'react'
+//import PropTypes from "prop-types"
+import '../../assets/styles/components/card.sass'
+
+const Card = (component_prop) => {
+    return (
+        <div className={`card-container`}>
+            
+        </div>
+    )
+}
+
+export default Card
