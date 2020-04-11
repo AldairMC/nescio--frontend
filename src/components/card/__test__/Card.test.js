a332260f5108d559a6a8527297708981e48306bd
Nicolas Camacho <ncamacho1@cuc.edu.co>
2020-04-11 18:46:01 -0500

    :white_check_mark:(develop) Card's test updated

--- src/components/card/__test__/Card.test.js
+++ src/components/card/__test__/Card.test.js
@@ -1,11 +1,17 @@
 import React from "react"
+import { mount, configure } from "enzyme"
 import renderer from "react-test-renderer"
-
+//Component
 import Card from "../Card"
 
 describe("<Card></Card>", () => {
   test("Renders correctly", () => {
     const card = renderer.create(<Card></Card>).toJSON()
     expect(card).toMatchSnapshot()
+  })
+
+  test("Mount Correctly", () => {
+    const wrapper = mount(<Card></Card>)
+    expect(wrapper.length).toEqual(1)
   })
 })
